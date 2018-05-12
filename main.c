43f62139fed1300e5630711a84d4c7687beb0b5a
MasMasFamas <cutypiano@naver.com>
2018-05-12 19:22:49 +0900

    text1 text2 informaion complete

--- main.c
+++ main.c
@@ -61,6 +61,16 @@ int main(void)
 void filestat1(void)
 
 {
+	stat("text1", &stat1);
+	time1 = localtime(&stat1.st_mtime);
+	printf("text1 information\n");
+	printf("size:%d\n", (int)stat1.st_size);
+	printf("blocks:%d\n", (int)stat1.st_blocks);
+	printf("month:%d\n", time1->tm_mon+1);
+	printf("date:%d\n", time1->tm_mday);
+	printf("hour:%d\n", time1->tm_hour);
+	printf("min:%d\n", time1->tm_min);
+	printf("\n");
 
 }
 
@@ -69,7 +79,16 @@ void filestat1(void)
 void filestat2(void)
 
 {
-
+	stat("text2", &stat2);
+	time2 = localtime(&stat2.st_mtime);
+	printf("text2 information\n");
+	printf("size:%d\n", (int)stat2.st_size);
+	printf("blocks:%d\n", (int)stat2.st_blocks);
+	printf("month:%d\n", time2->tm_mon+1);
+	printf("date:%d\n", time2->tm_mday);
+	printf("hour:%d\n", time2->tm_hour);
+	printf("min:%d\n", time2->tm_min);
+	printf("\n");
 }
 
  
