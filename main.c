5473a3053a86fc355e5206c5b9c36f1240a03cb6
LIM-youn-soo <maekung3@gmail.com>
2018-05-12 19:20:28 +0900

    filetime1, filetime2 are implemented

--- main.c
+++ main.c
@@ -59,7 +59,6 @@ int main(void)
  
 
 void filestat1(void)
-
 {
 
 }
@@ -72,22 +71,21 @@ void filestat2(void)
 
 }
 
- 
-
 void filetime1(void)
-
 {
-
+        stat("text1", &stat1);
+        time1 = localtime(&stat1.st_mtime);
+        printf("text1's time info: %d.%d %d:%d\n ",time1->tm_mon+1, time1->tm_mday, time1->tm_hour, time1->tm_min);
 }
-
- 
 
 void filetime2(void)
-
 {
+        stat("text2", &stat2);
+        time2 = localtime(&stat2.st_mtime);
+        printf("text2's time info: %d.%d %d:%d\n",time2->tm_mon+1, time2->tm_mday, time2->tm_hour, time2->tm_min);
+
 
 }
-
  
 
 void sizecmp(void)
